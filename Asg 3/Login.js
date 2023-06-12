e42735c514ebabbc8c53b1db0af22b8e0fbae7fe
Maryam Amjad <87093456+MaryamAmjad2@users.noreply.github.com>
2023-06-12 10:45:15 -0700

    Update Login.js

--- Asg 3/Login.js
+++ Asg 3/Login.js
@@ -1,52 +1,66 @@
 import * as React from 'react';
-import { Text, View, StyleSheet,TextInput,TouchableOpacity,Image } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import Constants from 'expo-constants';
 import AntDesign from '@expo/vector-icons/AntDesign';
 
-
-const LogIn=()=> {
+const LogIn = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <View style={{ flex: 1}}>
-        <View style={{ flex: 1, marginTop:20 }}>
-        <AntDesign name="arrowleft" size={32} color="blue" />
-        </View>
-        <View style={{ flex: 2,   }}>
+      <View style={{ flex: 1 }}>
         <Text style={styles.paragraph}> Welcome !</Text>
-        <Text style={styles.greyText}>
-        Sign in to Continue
-        </Text>
-        </View>
+        <Text style={styles.greyText}>Sign in to Continue</Text>
       </View>
-      <View style={{ flex: 3,justifyContent: 'center',
-    alignItems: 'center'}}>
-     
-      <TextInput style={styles.inp} placeholder='maryam@gmail.com' keyboardType="email-address"></TextInput>
-      <TextInput style={styles.inp2} secureTextEntry={true} />
-      <TouchableOpacity  style={styles.loginButton}>
-      <Text style={styles.buttonText}> Login</Text>
-      </TouchableOpacity>
-      <Text style={styles.forgotText}> Forgot Password</Text>
-       <Text style={styles.forgotText}>
-       ____________________  <Text style={{color:'#00008B'}}>OR</Text>  ___________________
-       </Text>
-      
+      <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center' }}>
+        <TextInput
+          style={styles.inp}
+          placeholder="Enter Email"
+          keyboardType="email-address"></TextInput>
 
+        <TextInput
+          style={styles.inp2}
+          secureTextEntry={true}
+          placeholder="Enter Password"
+        />
+
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => navigation.navigate('Profile')}>
+          <Text style={styles.buttonText}> Login</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.forgotText}> Forgot Password</Text>
+        <Text style={styles.forgotText}>
+          ____________________ <Text style={{ color: '#00008B' }}>OR</Text>{' '}
+          ___________________
+        </Text>
       </View>
-      <View style={{ flex: 1.5,justifyContent: 'center',
-    alignItems: 'center',marginTop:-80}}>
-      <Text style={styles.greyText}> Social Media Login</Text>
-      <View style={{flexDirection:'row',marginTop:10,padding:30}}>
-      <Image source={require('google.png')} style={{marginRight:10}}/>
-       <AntDesign name="facebook-square" size={32} color="blue" />
-       <AntDesign name="apple1" size={32} color="black" />
+      <View
+        style={{
+          flex: 1.5,
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: -80,
+        }}>
+        <Text style={styles.greyText}> Social Media Login</Text>
+        <View style={{ flexDirection: 'row', marginTop: 10, padding: 30 }}>
+          <AntDesign name="facebook-square" size={32} color="blue" />
+          <AntDesign name="apple1" size={32} color="black" />
+        </View>
+        <Text style={{ fontSize: 12 }}>
+          Dont have an account ?<Text style={{ color: 'blue' }}>Sign Up</Text>
+        </Text>
       </View>
-      <Text style={{fontSize:12}}>Dont have an account ?<Text style={{color:'blue'}}>Sign Up</Text></Text>
-      </View>
-      <View style={{flex:0.23}}></View>
+      <View style={{ flex: 0.23 }}></View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -58,41 +72,38 @@ const styles = StyleSheet.create({
     marginRight: 20,
   },
   paragraph: {
-    marginTop:10,
+    marginTop: 10,
     fontSize: 30,
     fontWeight: 'bold',
-    letterSpacing:2
-    
+    letterSpacing: 2,
   },
-    greyText: {
+  greyText: {
     marginLeft: 3,
     fontSize: 15,
     color: 'gray',
-    marginTop:5,
-    
-    
+    marginTop: 5,
   },
-  inp:{
-borderWidth:2,
-borderColor: "gray",
-    width: "100%",
+  inp: {
+    borderWidth: 2,
+    borderColor: 'gray',
+    width: '100%',
     borderRadius: 10,
     padding: 10,
-     marginTop:30,
-     color:'#00008B',
-     marginTop:-15
+    marginTop: 30,
+    color: '#00008B',
+    marginTop: -15,
   },
-    
-  inp2:{
-borderWidth:2,
-borderColor: "gray",
-    width: "100%",
+
+  inp2: {
+    borderWidth: 2,
+    borderColor: 'gray',
+    width: '100%',
     borderRadius: 10,
     padding: 10,
-    marginTop:30,
-    color:'#00008B'
+    marginTop: 30,
+    color: '#00008B',
   },
-     loginButton: {
+  loginButton: {
     textAlign: 'center',
     justifyContent: 'center',
     alignItems: 'center',
@@ -101,22 +112,20 @@ borderColor: "gray",
     borderRadius: 3,
     paddingVertical: 10,
     marginTop: 20,
-    width:250
+    width: 250,
   },
-    buttonText: {
+  buttonText: {
     fontSize: 15,
     color: 'white',
     alignSelf: 'center',
     textTransform: 'uppercase',
     letterSpacing: 3,
-
   },
-  forgotText:{
-    fontSize:12,
-    color:'grey',
-    marginTop:10
-  }
-  }
-);
+  forgotText: {
+    fontSize: 12,
+    color: 'grey',
+    marginTop: 10,
+  },
+});
 
-export default LogIn
+export default LogIn;
